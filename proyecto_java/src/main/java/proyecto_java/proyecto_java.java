a726549f37bc7591c91995267c890bb009a9ab02
MarisolOrtiz <64797672+MarisolOrtiz@users.noreply.github.com>
2020-05-16 18:43:45 -0600

    estes es mi commit

---
+++ proyecto_java/src/main/java/proyecto_java/proyecto_java.java
@@ -0,0 +1,40 @@
+
+package proyecto_java;
+public class proyecto_java {
+    public static void main(String[] args) {
+     
+   
+          double dinero =100;
+                   double vrl; 
+                   double vr2;
+                   double vr3; 
+                   double vr4; 
+                   double vr5;
+                   double US =1;
+                   double Europa = 0.70; 
+                   double ReinoUnido = 0.61;
+                   double Australia = 0.95;
+                   double  canada = 0.97;
+                 
+                  vrl = (dinero* US);
+                  vr2 = (dinero* Europa );
+                  vr3= (dinero* ReinoUnido);
+                  vr4 = (dinero* Australia );
+                  vr5 = (dinero* canada);
+                    
+               
+                     System.out.println ("aconversion  a eu" + vrl);
+                   System.out.println ("aconversion  a Europa" + vr2);
+                  System.out.println ("aconversion  a ReinoUnido" + vr3);
+                  System.out.println ("aconversion  a Australia " + vr4);
+                     System.out.println ("aconversion  a canada" + vr5);
+                     
+                           
+                  
+    }
+        
+        
+        
+        
+    }
+}
